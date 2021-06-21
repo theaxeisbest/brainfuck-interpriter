3963420940c623f49c9093aa385c2ce79a83e4cb
theaxeisbest <axelwas07@gmail.com>
2021-06-21 13:37:16 +0200

    made it 100 % faster but there is unused mem

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+
 char* LoadFile(const char* fileName, size_t* len){
     char * buffer = 0;
     unsigned long long length;
@@ -27,45 +29,138 @@ char* LoadFile(const char* fileName, size_t* len){
     return 0;
 }
 
-void interpriter(char* file) 
+char* Optimise(char* file)
 {
     unsigned long long int length = strlen(file);
+
+    unsigned long long int size = 0;
+    unsigned long long int realSize = 0;
+
+    for (unsigned long long int n = 0; n < length; n++)
+    {
+        if (file[n] == '<' || 
+            file[n] == '>' || 
+            file[n] == '+' || 
+            file[n] == '-' )
+        {
+            size += 2;
+            if (file[n] != file[n - 1])
+                realSize += 2;
+        } 
+        else if (file[n] == '[' ||
+                 file[n] == ']' ||
+                 file[n] == '.' ||
+                 file[n] == ',')
+        {
+            size++;
+            realSize++;
+        }
+    }
+    char* optimizedPrg = (char*)malloc(size + 1);
+
+    optimizedPrg[size] = '\0';
+
+    unsigned long long int prgWriteHead = 0;
+
+    unsigned long long int n = 0;
+
+    while (n <= size)
+    {
+        if (file[n] == '[' ||
+            file[n] == ']' ||
+            file[n] == '.' ||
+            file[n] == ',')
+        {
+            optimizedPrg[prgWriteHead] = file[n];
+            n++;
+            prgWriteHead++;
+        }
+        else if (file[n] == '<' || 
+                 file[n] == '>' || 
+                 file[n] == '+' || 
+                 file[n] == '-' )
+        {
+
+            optimizedPrg[prgWriteHead] = file[n];
+            prgWriteHead++;
+            optimizedPrg[prgWriteHead] = 1;
+
+            n++;
+
+            while (file[n] == file[n - 1] && n <= size && optimizedPrg[prgWriteHead] < 90)
+            {
+                optimizedPrg[prgWriteHead]++;
+                n++;
+            }
+            prgWriteHead++;
+        }
+        else
+        {
+            n++;
+        }
+    }
+    optimizedPrg[realSize + 1] = '\0';
+
+    return optimizedPrg; 
+    
+}
+
+unsigned long long int GetPrgSize(char* prg)
+{
+    unsigned long long int ret = 0;
+
+    while (prg[ret] != 0)
+    {
+        ret++;
+    }
+
+    return ret;
+}
+
+void Interpriter(char* prg) 
+{
+    unsigned long long int length = GetPrgSize(prg);
     #define TAPE_LENGTH 1000000
     char* tape = malloc(TAPE_LENGTH);
     unsigned long long int memPointer = 0;
     unsigned long long int prgPointer = 0; //the instruction we are on
     unsigned int numStackedLoops = 0;
     
-    while (prgPointer < length + 1)
+    while (prgPointer < length)
     {
         
         //printf("not here!\n");
-        switch (file[prgPointer])
+        switch (prg[prgPointer])
         {
         case '+':
-            tape[memPointer] += 1;
+            tape[memPointer] += prg[prgPointer + 1];
+            prgPointer++;
             break;
         case '-':
-            tape[memPointer] -= 1;
+            tape[memPointer] -= prg[prgPointer + 1];
+            prgPointer++;
             break;
         case '<':
-            memPointer -= 1;
+            memPointer -= prg[prgPointer + 1];
             if (memPointer > TAPE_LENGTH)
             {
-                printf("mem pointer out of bounds\n\n EXITING...\n");
+                printf("mem pointer out of bounds <\n\n EXITING...\n");
                 exit(-2);
             }
+            prgPointer++;
             break;
         case '>':
-            memPointer += 1;
+            memPointer += prg[prgPointer + 1];
             if (memPointer > TAPE_LENGTH)
             {
-                printf("mem pointer out of bounds\n\n EXITING...\n");
+                printf("mem pointer out of bounds >\n\n EXITING...\n");
                 exit(-2);
             }
+            prgPointer++;
             break;
         case '.':
             printf("%c", tape[memPointer]);
+            break;
         case '[':
             numStackedLoops++;
 
@@ -75,11 +170,11 @@ void interpriter(char* file)
                 while (numStackedLoopsEnd != numStackedLoops - 1)
                 {
                     prgPointer++;
-                    if (file[prgPointer] == '[')
+                    if (prg[prgPointer] == '[')
                     {
                         numStackedLoopsEnd++;
                     } 
-                    else if (file[prgPointer] == ']')
+                    else if (prg[prgPointer] == ']')
                     {
                         numStackedLoopsEnd--;
                     }
@@ -97,11 +192,11 @@ void interpriter(char* file)
                 while (numStackedLoopsEnd != numStackedLoops + 1)
                 {
                     prgPointer--;
-                    if (file[prgPointer] == '[')
+                    if (prg[prgPointer] == '[')
                     {
                         numStackedLoopsEnd++;
                     } 
-                    else if (file[prgPointer] == ']')
+                    else if (prg[prgPointer] == ']')
                     {
                         numStackedLoopsEnd--;
                     }
@@ -128,6 +223,7 @@ void interpriter(char* file)
     return;
 }
 
+
 int main(int argc, char *argv[]) 
 {
 
@@ -142,11 +238,13 @@ int main(int argc, char *argv[])
 
     char* file = LoadFile(argv[1], length);
 
-    
-
-    interpriter(file); 
+    char* optimizedPrg = Optimise(file);
 
     free(file);
+
+    Interpriter(optimizedPrg); 
+
+    free(optimizedPrg);
     free(length);
 
     return 0;
